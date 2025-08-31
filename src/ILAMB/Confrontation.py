5723f7bc9beca90c6ce96b057c46ac1fa7e7f35f
Patrick McGuire <p.mcguire@reading.ac.uk>
2025-08-31 21:02:13 +0100

    updated keywords

--- src/ILAMB/Confrontation.py
+++ src/ILAMB/Confrontation.py
@@ -461,6 +461,7 @@ class Confrontation(object):
             skip_rmse = self.keywords.get("skip_rmse", False)
             skip_iav = self.keywords.get("skip_iav", True)
             skip_cycle = self.keywords.get("skip_cycle", False)
+            biasscore_switch = self.keywords.get("biasscore_switch", False)
             rmse_score_basis = self.keywords.get("rmse_score_basis", "cycle")
             if obs.spatial:
                 il.AnalysisMeanStateSpace(
@@ -476,6 +477,7 @@ class Confrontation(object):
                     skip_iav=skip_iav,
                     skip_cycle=skip_cycle,
                     mass_weighting=mass_weighting,
+                    biasscore_switch=biasscore_switch,
                     rmse_score_basis=rmse_score_basis,
                     df_errs=self.df_errs,
                 )
@@ -493,6 +495,7 @@ class Confrontation(object):
                     skip_iav=skip_iav,
                     skip_cycle=skip_cycle,
                     mass_weighting=mass_weighting,
+                    biasscore_switch=biasscore_switch,
                     df_errs=self.df_errs,
                 )
             fcm.mod_dset.setncattr("complete", 1)
